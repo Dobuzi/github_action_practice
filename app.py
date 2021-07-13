0418fce8f5167ea1ebb968ee015812d1255cd423
Dobuzi <kimjw7981@gmail.com>
2021-07-13 13:22:55 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, json
+import logging
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    app.logger.info('Main request successful')
+    return "Hello World!"
+
+@app.route("/status")
+def status():
+    response = app.response_class(
+                response=json.dumps({"result": "OK - healthy"}),
+                status=200,
+                mimetype='application/json'
+               )
+    app.logger.info('Status endpoint was reached')
+    return response
+
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+                response=json.dumps({"status": "success",
+                                     "data": {
+                                              "UserCount": 140,
+                                              "UserCountActive": 23
+                                             }
+                                    }),
+                status=200,
+                mimetype='application/json'
+               )
+    app.logger.info('Metrics endpoint was reached')
+    return response
+
+if __name__ == "__main__":
+    logging.basicConfig(filename="app.log", level=logging.DEBUG)
+    app.run(host='0.0.0.0')
